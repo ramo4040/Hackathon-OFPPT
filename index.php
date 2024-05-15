1b4ff875ac4de5a70fd098a7534045d8295198e8
Yassir <97519266+ramo4040@users.noreply.github.com>
2024-05-15 17:59:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,15 @@
 ini_set('session.use_only_cookies', 1);
 ini_set('session.use_strict_mode', 1);
 
+session_set_cookie_params([
+    'lifetime' => 0,
+    'domain' => 'localhost',
+    'path' => '/',
+    'secure' => true,
+    'httponly' => true,
+]);
+
+
 require_once 'App/Config/Init.php';
 require_once 'App/Helper/Autoloader.php';
 require_once 'App/Config/Container.php';
