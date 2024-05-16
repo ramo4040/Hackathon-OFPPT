66311c40391763ea1defd0efee76b5f155445a0b
Yassir <97519266+ramo4040@users.noreply.github.com>
2024-05-16 19:50:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-
-
 <?php
+
+
 ini_set('session.use_only_cookies', 1);
 ini_set('session.use_strict_mode', 1);
 
@@ -12,16 +12,12 @@ session_set_cookie_params([
     'httponly' => true,
     'samesite' => 'Strict' // Helps mitigate CSRF attacks
 ]);
-session_name('s_h_session_id');
-
 
 require_once 'App/Config/Init.php';
-require_once 'App/Helper/Autoloader.php';
+require_once 'vendor/autoload.php'; // autoloader
 require_once 'App/Config/Container.php';
 require_once 'App/Routes/api.php';
+
 session_start();
 
 App\Routes\Router::route($_SERVER['REQUEST_METHOD']);
-
-
-?>
